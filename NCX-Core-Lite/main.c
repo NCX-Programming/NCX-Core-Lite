6946d3586bf172b2260d310afce45842ae05e303
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-02-28 18:10:59 -0500

    Added more basic code

--- NCX-Core-Lite/main.c
+++ NCX-Core-Lite/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+// Declare functions
+int Menu();
+// Start code
+int clrScrn() {
+    // Screen clear fucntion, checks OS first for compatibility:tm:
+    #ifdef _WIN32
+        system("cls");
+    #endif
+    #ifdef unix
+        system("clear");
+    #endif
+    #ifdef __APPLE__
+        system( "clear" );
+    #endif
+}
+int Menu(){
+    
+}
 int main(){
     printf("Hello World!\n");
 }
