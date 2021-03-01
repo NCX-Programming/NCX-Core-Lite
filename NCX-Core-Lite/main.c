6357f953c52a8ed12cac9423d2de3efd8931ac0a
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-03-01 10:26:34 -0500

    Added some download code

--- NCX-Core-Lite/main.c
+++ NCX-Core-Lite/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <string.h>
 // Declare functions
 int Menu();
+int Download();
+// Declare variables
+char url[1024] = "http://localhost/aaa.txt";
+// Not really sure what this code does but it helps with downloading stuff so its important
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
 // Start code
 int clrScrn() {
     // Screen clear fucntion, checks OS first for compatibility:tm:
@@ -16,9 +27,27 @@ int clrScrn() {
         system( "clear" );
     #endif
 }
+int Download(){
+    // Download code
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    char outfilename[FILENAME_MAX] = "C:\\bbb.txt";
+    curl = curl_easy_init();
+    if (curl) {
+        fp = fopen(outfilename,"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+        fclose(fp); }
+}
 int Menu(){
     
 }
 int main(){
     printf("Hello World!\n");
+    Download();
 }
